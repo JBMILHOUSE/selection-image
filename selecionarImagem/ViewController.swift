abde17bb63a10b2cef0890ac84467bd7a81563a9
Chris <43553799+JBMILHOUSE@users.noreply.github.com>
2020-11-23 14:16:46 -0500

    add localizacao

--- selecionarImagem/ViewController.swift
+++ selecionarImagem/ViewController.swift
@@ -6,19 +6,35 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
+    var localizacao : CLLocation!
+    
     @IBOutlet weak var ivImage: UIImageView!
+    
+    @IBOutlet weak var lbLat: UILabel!
+    
+    @IBOutlet weak var lbLgt: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        locationManager.requestWhenInUseAuthorization()
     }
     
     @IBAction func selecionarImagem(_ sender: Any) {
         file().selecionaImagem(self) {
             ivImage in self.ivImage.image = ivImage
+        }
+    }
+    @IBAction func selecionadorCoordenadas(_ sender: Any) {
+        if(CLLocationManager.authorizationStatus() == .authorizedWhenInUse) || (CLLocationManager.authorizationStatus() == .authorizedAlways) {
+            localizacao = locationManager.location
+            lbLat.text = "Lat : \(localizacao.coordinate.latitude)"
+            lbLgt.text = "Lgt : \(localizacao.coordinate.longitude)"
         }
     }
 }
